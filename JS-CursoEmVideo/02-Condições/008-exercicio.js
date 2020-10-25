54925d037d356e9e6c264c036348cd6d74d0bad3
Bruno Theodoro <theodorodeaquino@gmail.com>
2020-10-25 08:07:42 -0300

    Aula 11 - curso em vídeo

---
+++ JS-CursoEmVideo/02-Condições/008-exercicio.js
@@ -0,0 +1,7 @@
+let velocidade = 70
+
+console.log(`A velocidade que você está é ${velocidade}`)
+
+if (velocidade > 60){
+    console.log(`Sua velocidade é ${velocidade} ultrapassando em ${velocidade - 60} o permitido. Multado`)
+}
